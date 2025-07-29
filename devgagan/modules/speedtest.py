c1fd19c3c4ddd9ea4f2fa5dad0ce5e9d85602b43
Ankush796 <r2131581@gmail.com>
2025-07-29 11:05:45 +0530

    Update speedtest.py

--- devgagan/modules/speedtest.py
+++ devgagan/modules/speedtest.py
@@ -2,9 +2,9 @@
 # File Name: speedtest.py
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Gagan
+# Author: Team Leader
 # GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
+# Telegram: https://t.me/+kuVOUv7m_dRmM2Vl
 # YouTube: https://youtube.com/@dev_gagan
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
